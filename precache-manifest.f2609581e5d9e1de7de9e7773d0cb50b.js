470249f376187cf7ac52e08c2da4d4274311bfec
Harish Mamidi <harish.mamidi.kumar@gmail.com>
2020-06-06 16:03:08 +0530

    Updates

---
+++ precache-manifest.f2609581e5d9e1de7de9e7773d0cb50b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dceb8e651794dd24f97a70e515fa02d",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "125f25a7de4a5933c197",
+    "url": "/monster/static/css/main.c9c50fa4.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monster/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125f25a7de4a5933c197",
+    "url": "/monster/static/js/main.11100cfc.chunk.js"
+  },
+  {
+    "revision": "ce4106392ac25c68b11d",
+    "url": "/monster/static/js/runtime-main.9dda15a6.js"
+  }
+]);
