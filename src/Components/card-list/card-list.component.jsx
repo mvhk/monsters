a07f209a20fd5508837494ff070a8d595390eefb
Harish Mamidi <harish.mamidi.kumar@gmail.com>
2020-06-06 12:44:40 +0530

    Search Field added

--- src/Components/card-list/card-list.component.jsx
+++ src/Components/card-list/card-list.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./card-list.styles.css";
 import { Card } from "../card/card.component";
+
 export const CardList = (props) => {
   return (
     <div className="card-list">
